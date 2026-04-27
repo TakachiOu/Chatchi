84e0943b0432f6b699d012864d0629dfe81f4ad9
Abdou Ouassaa <abdououasaa@gmail.com>
2026-04-27 09:38:18 +0100

    1.9v

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/privacy', (req, res) => {
 // ==========================================
 app.get('/api/app-version', (req, res) => {
     res.json({
-        latestVersion: "1.0.0", // رقم الإصدار المطلوب حالياً (يمكنك تغييره مستقبلاً)
+        latestVersion: "1.1.0", // رقم الإصدار المطلوب حالياً (يمكنك تغييره مستقبلاً)
         forceUpdate: true, // هل التحديث إجباري؟ (true يعني نعم)
         playStoreUrl: "https://play.google.com/store/apps/details?id=com.chatchi.app" // رابط تطبيقك على متجر بلاي (يُعدل لاحقاً)
     });
